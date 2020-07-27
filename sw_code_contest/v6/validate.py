b9f73d3a3246b7f922c832c57cb787c916e1e027
Rishabh <rishabhjain7b@gmail.com>
2020-07-28 00:20:03 +0530

    Support for showing extra & reduce %

--- sw_code_contest/v6/validate.py
+++ sw_code_contest/v6/validate.py
@@ -1,19 +1,23 @@
 #################################################
-# Date:         25 JUL'20    
+# Date:         27 JUL'20    
 # Author:       Rishabh Jain
 # Description:  Validate Structures of two files
 # Result:       Pass Percentage/Mismatch & 
 #               Run time of the design (Optional)                
 #################################################
 
-# DONE: Calculate execution runtime.
+# DONE: Calculate execution runtime in us.
+# DONE: Order change shows less %.
+# DONE: If extra present in input file show less %.
 
-# Known Issue:
-# 1. If mismatch, extra element in input file not shown.
+# Precedence:
+# 1. Incomplete File.
+# 2. Extra Data in File.
+# 3. Order Descrepancy in File.
 
 import sys
 import os
-import datetime
+import time
 import difflib
 import subprocess
 from collections import Counter
@@ -28,7 +32,7 @@ INPUT_FILE = sys.argv[1]
 #print (" Reading Golden File ")
 
 # Golden File
-GOLDEN_FILE = 'data.json'
+GOLDEN_FILE = 'design.txt'
 
 # Convert file into ordered list
 # Discard new lines, tabs and whitespaces
@@ -46,11 +50,18 @@ def comp(li1, li2):
 
 # Check for similarity and order
 # l1: golden, l2: input
+# return value=
+# 0 ==> Perfect match. 
+# 1 ==> Extra Data in Input File. (Order, Don't Care) 
+# 2 ==> Order discrepancy 
 def check_same(l1, l2):
-    if len(l1)==len(l2) and len(l1)==sum([1 for i, j in zip(l1, l2) if i==j]):
-        return True
+    if len(l1)==len(l2):
+        if len(l1)==sum([1 for i, j in zip(l1, l2) if i==j]):
+            return 0    
+        else:
+            return 2
     else:
-        return False
+        return 1
 
 # Calculate match percentage
 # l1: golden, l2: input
@@ -68,42 +79,53 @@ def percent(l1, l2):
 
 # Show differences between files (Missing or Unordered)
 # l1: golden, l2: input
+# return count for lessening the percentage
 def diff(f1, f2, match):
     diff = difflib.unified_diff(f1, f2, fromfile='inp', tofile='gold', lineterm='', n=0)
     lines = list(diff)[2:]
     added = [line[1:] for line in lines if line[0] == '+']
     removed = [line[1:] for line in lines if line[0] == '-']
+    count = 0
     
     print ("------------------------------")
     if match:
         identical = check_same(inp_file, gold_file)
-        if not identical:
-            print ("WARNING: Order Discrepancy ")
+        if identical == 2:
+            print ("ERROR: Order Discrepancy ")
             print ("INFO: Positional Ambiguity at:")
             for line in added:
                 if line in removed:
+                    count = count + 1
                     print "-->", line
+        elif identical == 1:
+            print ("ERROR: Exceeded Data Limit ")
+            print ("INFO: Extra Elements are:")
+            for line in added:
+                count = count + 1
+                print "-->", line
         else:
             print ("!! PASSED SUCCESSFULLY !!")
     else:
-        print ("INFO: Incomplete File")
+        print ("ERROR: Incomplete File")
         print ("INFO: Missing Elements are:")
         for line in removed:
             print "-->", line
+    
+    return count
 
 # Calculate execution time of the design
 # If second argument is provided.
 def exec_time(command):
-    start_time = datetime.datetime.now()
+    start_time = time.time()
 
     with open(os.devnull, "w") as fnull:
         result = subprocess.call(command, stdout = fnull, stderr = fnull)
     
-    end_time = datetime.datetime.now()
+    end_time = time.time()
     time_taken = end_time - start_time
 
     print ("\n---- RUNTIME TIME ELAPSED -----")
-    print '\t ', time_taken.total_seconds() * 1000,'ms'
+    print '\t ', round(time_taken * 1000 * 1000, 2),'us'
 
 # Converting files 
 inp_file = file_list(INPUT_FILE)
@@ -113,9 +135,6 @@ gold_file = file_list(GOLDEN_FILE)
 #print (" Comparing the Files... \n")
 match_cent = percent(gold_file, inp_file)
 
-print ("------ PASS PERCENTAGE -------")
-print '\t  ', match_cent,'% \n'
-
 # Check for full match
 if match_cent == 100:
     file_match = True
@@ -123,9 +142,12 @@ else:
     file_match = False
 
 # Show the differences
-diff(gold_file, inp_file, file_match)
+cent_diff = diff(gold_file, inp_file, file_match)
 
 print ("------------------------------")
+
+print ("\n------ PASS PERCENTAGE -------")
+print '\t  ', match_cent - cent_diff,'%'
 
 # Check for 2nd Argument and check runtime
 if len(sys.argv) > 2:
